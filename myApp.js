5d99207c02bdb06d90d33947b13e4076b85caa89
ramidevictoria <>
2022-01-09 23:52:20 +0000

    Added the findOneAnUPdate function

--- myApp.js
+++ myApp.js
@@ -92,7 +92,12 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, function(err,person){
+    if (err) return console.error(err);
+    done(null, person);
+  });
+
+  
 };
 
 const removeById = (personId, done) => {
