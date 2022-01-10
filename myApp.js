9176f4d0d3818d09bd1e7d745d51a74b875104dc
ramidevictoria <>
2022-01-10 01:15:37 +0000

    Add chained find query | Last Commit

--- myApp.js
+++ myApp.js
@@ -125,7 +125,17 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person
+    .find({favoriteFoods: foodToSearch})
+    .sort({name: 1})
+    .limit(2)
+    .select({age: 0})
+    .exec((err, data) => {
+      if (err) return console.error(err);
+      console.log(data);
+      done(null, data);
+    });
+  
 };
 
 /** **Well Done !!**
